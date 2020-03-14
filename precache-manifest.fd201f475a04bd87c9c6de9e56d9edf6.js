b8898c89d2dde449e51c0d246f488fed5355a659
ivargas1030 <56612683+ivargas1030@users.noreply.github.com>
2020-03-14 17:54:45 -0500

    Updates

---
+++ precache-manifest.fd201f475a04bd87c9c6de9e56d9edf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce276fb67ae44f52b871a3630389999f",
+    "url": "/ivargas1030/employeedirectory.git/index.html"
+  },
+  {
+    "revision": "54c2e1c6e891e033080a",
+    "url": "/ivargas1030/employeedirectory.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ec66df74d7cb62078e7",
+    "url": "/ivargas1030/employeedirectory.git/static/js/2.7434759e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ivargas1030/employeedirectory.git/static/js/2.7434759e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c2e1c6e891e033080a",
+    "url": "/ivargas1030/employeedirectory.git/static/js/main.93bf94d0.chunk.js"
+  },
+  {
+    "revision": "f74c07eb11d0656b1e65",
+    "url": "/ivargas1030/employeedirectory.git/static/js/runtime-main.9f1ab6a6.js"
+  }
+]);
